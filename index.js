4cc55bae94f6f61d0c84e4d059cc62b6d3b97046
Hugo Alberto Garcia De la Pena <hgarc@softserveinc.com>
2023-03-20 19:25:59 -0600

    homework3

--- index.js
+++ index.js
@@ -1,141 +1,144 @@
-/**1.- Add the code it is neccesary to use the variable x x and return the following  */
-let x = 1
-let y = 2
-let res1 = x.toString() + y.toString()
-console.log(res1)// String "12"
-console.log(typeof res1)// string
-let res2 = (y > x || x > y).toString() + y.toString()
-console.log(res2)// string "true2"
-console.log(typeof res2)// string
-let res3 = (y > x || x > y)
-console.log(res3)// true 
-console.log(typeof res3)// boolean
-let res4 = 1 / "a"
-console.log(res4) // Nan
-console.log(typeof res4) // number
-console.log("*******************************************************next exercise")
 
-/** 2.- The user enters a number (use prompt ()) . It is necessary 
- * to check whether the entered number is correct  */
+/**1.- We have the array with elements [2, 3, 4, 5]. Use the loop “for” to
+ *  find the product of the elements of this array. Also implement tasks through “while” loop .  */
+let arrayNumbers = [2, 3, 4, 5]
+let productOfCicleFor = 1
+let productOfCicleWhile = 1
+let x = 0
 
-let m = parseInt(prompt("Enter a number"))
-if (!isNaN(m)) {
-    console.log("The value is a number")
-
-    if (Math.sign(m) == 1) {
-        console.log(m + " is positive")
-    } else {
-        console.log(m + " is negative")
-    }
-    if ((m % 7) == 0) {
-        console.log(m + "  is multiple of 7")
-    } else {
-        console.log(m + "  is not multiple of 7")
-    }
+for (let i = 0; i < arrayNumbers.length; i++) {
+    productOfCicleFor = productOfCicleFor * arrayNumbers[i]
 }
-else {
-    console.log("The value is not a  number")
+console.log("iteration of cicle for " + productOfCicleFor)
+
+while (x < arrayNumbers.length) {
+    productOfCicleWhile = productOfCicleWhile * arrayNumbers[x]
+    x++
 }
-console.log("*******************************************************next exercise")
+
+console.log("iteration of cicle while " + productOfCicleWhile)
+
+/**2.-  Write JavaScript “for” loop that will iterate from 0 to 15. For each iteration, 
+ * it will check whether the current number is even or odd and display a message on the screen. 
+ */
+for (let i = 0; i <= 15; i++) {
+    if ((i % 2) == 0) {
+        console.log(i + " is even")
+
+    } else {
+
+        console.log(i + " is odd")
+    }
+
+}
 
 
-/** 3. Create an array: */
-let arrayX = []
-// add number
-arrayX.push(2)
-    |// add a line
-    arrayX.push("this is the second element of the array")
-// add boolean
-arrayX.push(true)
-// add null
-arrayX.push(null)
-console.log("Array lenght " + arrayX.length)
-arrayX.push(prompt("Enter the next value of the array"))
-console.log("the fifth value of the array is " + arrayX[4])
-arrayX.shift()
-console.log("remove the first element of the array and print it Array= '" + arrayX + "'")
+/**3.- Write a function randArray ( k ) that will fill an array of k random integers. 
+ * Random numbers are generated from the range 1-500.  */
+let randArrayX = Math.random() * 100
+let arrayRandomInt = []
+for (let i = 0; i < randArrayX; i++) {
+    arrayRandomInt.push(Math.floor(Math.random() * 500))
+}
+console.log(arrayRandomInt)
+
+/**4.- Write the function raiseToDegree(a, b), 
+ * which returns the result of raising the number a to the power of b . 
+ * The function works only with integers. 
+ * Implement the interface for entering numbers a , b from the keyboard. 
+  */
+
+function raiseToDegree(a, b) {
+    return Math.pow(a, b)
+}
+alert("This program take two numbers and increase the base with the exponential number")
+let a = Number(prompt("Please Enter the base number "))
+let b = Number(prompt("Please enter the exponencial number"))
+
+if (isNaN(a) || isNaN(b)) {
+    alert("One or more numbers  entered  are invalid")
+} else {
+    alert("The result is " + raiseToDegree(a, b))
+}
 
 /**
- * 4.- Write a program that combines all existing elements of the array into a ribbon and separates them with the symbol “*”.
+ * 5 . Implement the findMin() function , which should accept an arbitrary number of numeric arguments 
+ * and return the one with the smallest value. Tip: you can use the special arguments 
+ * array for the solution . 
+ */
+let askContinue
+let arrayInput = []
+function findMin(arr) {
+    return Math.min.apply(Math, arr)
+}
+alert("This program will calculate the min value of the numbers that you enter")
+do {
+    arrayInput.push(parseInt(prompt("Enter a number")))
+    askContinue = prompt("Do you want to enter another number, type : 'Yes' or 'No'")
+
+}
+while (askContinue == "Yes" || askContinue == "yes" || askContinue == "y")
+
+alert("The min number of the list you enter is " + findMin(arrayInput))
+
+/**
+ * 6.- Write the function findUnique(arr), which accepts an array “arr” and 
+ * checks if all elements of array are unique ( no doubles ), 
+ * then function will return true if all elements are unique, otherwise - false. 
  */
 
-let cities = ["Mexico", "Los Angeles", "New York"]
-console.log(cities[0] + "*" + cities[1] + "*" + cities[2])
-// option two
-let newString = cities.join(" * ")
-console.log(newString)
+let askCont
+let arrayInputB = []
+function findDuplicates(arrayB) {
 
-/**5.- Using the prompt () function , ask the user a question about reaching the age of majority. 
- * Record the result in the isAdult variable . Depending on the received value, display the }
- * appropriate message about the person's status. 
- * For example: if the condition is fulfilled, the age is more than 18, output 
- * "You have reached the age of majority". If the age is less than 18 years, display "You are still too young",  */
-let ageMajority = parseInt(prompt("Enter your age"))
+    for (let i = 0; i < arrayB.length; i++) {
+        for (let j = 1; j < arrayB.length; j++) {
+            if (arrayB[i] == arrayB[j]) {
+                alert("There is a duplicate value ")
+                return;
+            } else {
+                alert("No duplicates")
+                return;
+            }
 
-if (ageMajority >= 18) {
-    alert("You have reached the age of majority")
-} else {
-    alert("You are still to young")
+        }
+    }
+
+}
+//const toFindDuplicates = arry => arry.filter((item, index) => arr.indexOf(item) !== index)
+
+alert("Excersice 6: The following script will tell you if you enter a duplicate number")
+
+do {
+    arrayInputB.push(parseInt(prompt("Enter a number")))
+    askCont = prompt("Do you want to enter another number, type : 'Yes' or 'No' ")
+
+}
+while (askCont == "Yes" || askCont == "yes" || askCont == "y")
+
+findDuplicates(arrayInputB)
+
+
+/**7.  Write a function that returns the last element of an array. 
+ * The function can accept 2 parameters: the 1st array, the 2nd numerical parameter corresponding to the number of "x" of 
+ * the last elements of the array to be output. 
+  */
+// pending to do, not sure what this exercise is about
+
+
+/**8 .- Write a function that takes a 
+ * string as an argument and converts the first letter of each word of the string to uppercase . 
+*/
+let stringInput = prompt("Enter a phrase , this program will update to uppercase each letter of the first word")
+
+let newStringArray = stringInput.split(" ")
+
+let newStringUpperCase=[]
+
+for (let stringValue of newStringArray) {
+    newStringUpperCase.push(stringValue.charAt(0).toUpperCase() + stringValue.slice(1))
 }
 
-/**6 .- The user enters the three lengths of the sides of the triangle (use prompt() three times to enter each side).  */
-let sideA = Number(prompt("Enter the length of the triangule base"))
-let sideB = Number(prompt("Enter the length of the triangule side"))
-let sideC = Number(prompt("Enter the length of the triangule the second side"))
-/**a ) determine and display the area of the triangle in the console  */
-if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
-    console.log("any of the enter sides is not a number")
-    console.log("operation can not be completed")
-} else {
-    let triPerimeter = sideA + sideB + sideC
-    let area = Math.sqrt(triPerimeter * ((triPerimeter - sideA) * (triPerimeter - sideB) * (triPerimeter - sideC)))
-    areaFixed = parseFloat(area).toFixed(3)
-    console.log("The area of the triangule is: " + areaFixed)
-}
-
-/**7   Write a conditional construction that, depending on the time of day, will display the 
-appropriate greeting. You need to get the real time of day from the system. 
-Do it in 2 ways through 2 different conditional operators. */
-let greeting;
-let systemDateToday = new Date()
-let systemTimeHour = systemDateToday.getHours()
-console.log(systemTimeHour)
-switch (systemTimeHour) {
-    case systemTimeHour <= 5 || systemTimeHour == 23:
-        alert("Hi this program wish you an excellent  Good night")
-        break;
-
-    case systemTimeHour > 5 || systemTimeHour <= 11:
-        alert("Hi this program wish you an excellent Good Morning")
-        break;
-
-    case systemTimeHour > 11 || systemTimeHour <= 17:
-        alert("Hi this program wish you an excellent Good Day")
-        break;
-
-    case systemTimeHour > 17 || systemTimeHour < 23:
-        alert("Hi this program wish you an excellent Good Evening")
-        break;
-}
-
-if (systemTimeHour <= 5 || systemTimeHour == 23) {
-    alert("Hi this program wish you an excellent Good Night")
-}
-
-if (systemTimeHour > 5 || systemTimeHour <= 11) {
-    alert("Hi this program wish you an excellent Good Morning")
-}
-
-if (systemTimeHour > 11 || systemTimeHour <= 17) {
-    alert("Hi this program wish you an excellent Good Day")
-}
-
-if (systemTimeHour > 17 || systemTimeHour < 23) {
-    alert("Hi this program wish you an excellent Good Evening")
-}
-
-
-
-
+alert("This is the new string " + newStringUpperCase.join(" "))
 
 
