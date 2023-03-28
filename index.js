0b34ef6b513d030ce8ff00fe4e122c2ec92c9f06
Hugo Alberto Garcia De la Pena <hgarc@softserveinc.com>
2023-03-28 14:12:41 -0600

    homework5

--- index.js
+++ index.js
@@ -1,133 +1,29 @@
-/**
- * 1.- Implement the calcRectangleArea(width, height) function, which takes 2 parameters, 
- * the width of the rectangle width and the height of the rectangle height, and calculates its area. 
- * Provide termination of program execution and generation of an exception if non-numeric parameters are passed to the function. 
- * Write code that uses this function and handles possible exceptions. 
- */
+/**1. Write the function propsCount(currentObject), which accepts an object 
+ * and determines the number of properties of this object .  */
 
-let area;
-function calcRectangleArea(width, height) {
-
-
-    try {
-        if (isNaN(width) || isNaN(height)) {
-            throw new Error("One of the inputs is invalid")
-        } else {
-            return width * height
-
-        }
-
-    }
-    catch (exception) {
-        return (exception.message)
-    }
+let mentor = {
+    course: "JS fundamental",
+    duration: 3,
+    direction: "web development"
 }
 
-let userWidhtInput = prompt("Enter the width of the rectangule")
-let userheighInput = prompt("Enter the height of the rectangule")
-area = calcRectangleArea(userWidhtInput, userheighInput)
-alert("The area of the rectangule is : " + area)
-
-
-/**2.- Write a checkAge() function that will prompt the user to enter their age and generate errors in the modal window */
-
-function checkAge(age) {
-    try {
-        if (age == "") {
-            throw new Error("The user enter an empty tape")
-        } else if (isNaN(age)) {
-            throw new Error("the enter data is not a number")
-        } else if (age < 14) {
-            throw new Error("You dont have enough age to view this movie")
-        } else {
-            return "You are allow to see the movie "
-        }
-    } catch (exception) {
-        return (exception.message)
+function propCount(mentor) {
+    let numberOfElements = 0
+    for (i in mentor) {
+        numberOfElements++
     }
 
-
+    return numberOfElements
 }
 
-let userAge = Number(prompt("Please enter your age"))
+alert("The number of elements in object mentor is " + propCount(mentor))
 
-alert(checkAge(userAge))
-
-/**3- Create a Month Exception class whose constructor takes a message parameter and initializes the name field to 'MonthException'. 
-Implement the showMonthName(month) function, where 
-the month parameter is the sequential number of the month of the year. 
-The function returns the name of the month according to the entered month number. 
-In case of incorrect input, an exception is thrown in the form of an object of the Month Exception 
-class with the message 'Incorrect month number'.  */
-class MonthName {
-    monthName=null
-
-    constructor(monthNumber) {
-        try {
-            console.log(monthNumber)
-            this.monthNumber = monthNumber
-            if (this.monthNumber > 12 || this.monthNumber == 0 || isNaN(monthNumber)) {
-                throw new Error("Incorrect month number")
-            } 
-        }
-        catch (exception) {
-            return (exception.message)
-        }
-
-    }
-
-     showMonthName(month) {
-        console.log(month)
-        switch (month) {
-            case 1:
-                return "Juanary"
-            case 2:
-                return "February"
-            case 3:
-                return "March"
-            case 4:
-                return "April"
-            case 5:
-                return "May"
-            case 6:
-                return "June"
-            case 7:
-                return "July"
-            case 8:
-                return "August"
-            case 9:
-                return "September"
-            case 10:
-                return "October"
-            case 11:
-                return "November"
-            case 12:
-                return "December"
-        }
+function Employee(firstName, lastName, position) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+    this.fullName = function () {
+        return this.firstName + " " + this.lastName;
     }
 }
-
-let monthUserInput = Number(prompt("Enter the number of the month you want to know the name"))
-let getNameMonth = new MonthName(monthUserInput)
-alert("The name of the month you enter is " + getNameMonth.showMonthName(monthUserInput))
-
-
-/**4. Implement the showUser ( id ) function, 
- * which takes the user id as a parameter and returns an 
- * object that contains the value of the passed id . The function also throws an error if a negative id is entered  */
-let idList= new Object()
-
-let userInput=[]
-
-function showUser(){
-    for(let i=0;i<userInput.length;i++){
-
-        try{
-            if ()
-
-        }catch(exception){
-
-        }
-
-    }
-}
+let devPeterPeterson = new Employee("Peter", "Peterson", "dev")
