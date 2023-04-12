3010854ce9b26fb50949696127039f8e060dc56b
Hugo Alberto Garcia De la Pena <hgarc@softserveinc.com>
2023-04-12 15:45:48 -0600

    homework9

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-/**1. Write the function propsCount(currentObject), which accepts an object 
- * and determines the number of properties of this object .  */
-
-let mentor = {
-    course: "JS fundamental",
-    duration: 3,
-    direction: "web development"
-}
-
-function propCount(mentor) {
-    let numberOfElements = 0
-    for (i in mentor) {
-        numberOfElements++
-    }
-
-    return numberOfElements
-}
-
-alert("The number of elements in object mentor is " + propCount(mentor))
-
-function Employee(firstName, lastName, position) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.position = position;
-    this.fullName = function () {
-        return this.firstName + " " + this.lastName;
-    }
-}
-let devPeterPeterson = new Employee("Peter", "Peterson", "dev")
-
-function Human(age, gender, nationality, name, lastName) {
-    this.age = age;
-    this.gender = gender;
-    this.nationality = nationality;
-    this.name = name;
-    this.lastName = lastName
-}
+/** 1.- Implement function getPromise ( message , delay ) which accepts textual message }
+ * and integer value delay ( in ms ) and returns a 
+* Promise which return a message after the entered delay.  */
 
 
-/**
- * 2. Create an arbitrary object that has 5 fields. It is necessary to write the showProps ( obj ) 
- * function , which accepts this object and outputs a list of its 
- * properties recorded in an array . Also enter an array of object property values
- */
+const myTimeout = setTimeout(myGreeting, 5000);
 
-const human= new Object()
+
+
+
+setTimeout(() => {
+    console.log("Delayed for 1 second.");
+  }, "1000");
