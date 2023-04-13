d73b798f9785d22eeeed1a2196eef33fba2f3761
Hugo Alberto Garcia De la Pena <hgarc@softserveinc.com>
2023-04-12 22:04:44 -0600

    homework5

--- index.js
+++ index.js
@@ -2,38 +2,38 @@
  * and determines the number of properties of this object .  */
 
 let mentor = {
-    course: "JS fundamental",
-    duration: 3,
-    direction: "web development"
+  course: "JS fundamental",
+  duration: 3,
+  direction: "web development"
 }
 
 function propCount(mentor) {
-    let numberOfElements = 0
-    for (i in mentor) {
-        numberOfElements++
-    }
+  let numberOfElements = 0
+  for (i in mentor) {
+    numberOfElements++
+  }
 
-    return numberOfElements
+  return numberOfElements
 }
 
 alert("The number of elements in object mentor is " + propCount(mentor))
 
 function Employee(firstName, lastName, position) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.position = position;
-    this.fullName = function () {
-        return this.firstName + " " + this.lastName;
-    }
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.position = position;
+  this.fullName = function () {
+    return this.firstName + " " + this.lastName;
+  }
 }
 let devPeterPeterson = new Employee("Peter", "Peterson", "dev")
 
 function Human(age, gender, nationality, name, lastName) {
-    this.age = age;
-    this.gender = gender;
-    this.nationality = nationality;
-    this.name = name;
-    this.lastName = lastName
+  this.age = age;
+  this.gender = gender;
+  this.nationality = nationality;
+  this.name = name;
+  this.lastName = lastName
 }
 
 
@@ -42,5 +42,64 @@ function Human(age, gender, nationality, name, lastName) {
  * function , which accepts this object and outputs a list of its 
  * properties recorded in an array . Also enter an array of object property values
  */
+let arrayOfProperties = []
+let arrayOfValues = []
 
-const human= new Object()
+let arbitraryObject = {
+  name: "Tester 1",
+  lastName: "Quality",
+  nationality: "Mexican",
+  age: 40,
+  gender: "Male"
+}
+
+function showProps(objTest) {
+
+  for (const i in objTest) {
+    arrayOfProperties.push(i)
+    arrayOfValues.push(objTest[i])
+
+  }
+}
+
+showProps(arbitraryObject)
+console.log(arrayOfProperties)
+console.log(arrayOfValues)
+
+/**3.-  Create the Person class , in which the constructor accepts the name and surname parameters , and also contains the showFullName () method, which displays the person's first and last name. 
+Student class is inherited from the Person class , the constructor of which, in addition to name and surname , accepts the year parameter (year of admission to the university).  */
+
+class Person {
+
+  constructor(name, surname) {
+    this.name = name
+    this.surname = surname
+    this.showFullName = function () {
+      return this.name + " " + this.surname
+    }
+
+  }
+}
+
+class Student extends Person {
+  constructor(name, surname, year, middlename) {
+    super(name, surname)
+    this.year = year
+    this.middlename = middlename
+    this.showFullName = function () {
+      return this.name + " " + this.surname + " " + this.middlename
+    }
+
+
+  }
+
+  showCourse() {
+    let currentYear = new Date().getFullYear()
+    return currentYear - year
+  }
+
+}
+
+let student = new Student("Hugo", "Garcia", 2014, "De la P")
+console.log(student.showFullName)
+console.log(student.showCourse)
